b4c4d1c1af1a1742df61b5ff3216c1a56d8fe10e
Andrew <asyermak@gmail.com>
2026-09-15 15:49:51 +0200

    added date, maybe will remove it later using std::time

---
+++ src/common/date.rs
@@ -0,0 +1,15 @@
+pub struct Date {
+    year: u32, //Only logical date, no need to use BC
+    month: u8,
+    day: u8,
+
+    hour: u8,
+    minute: u8,
+    second: u8,
+}
+
+//simply fmt to smthing like 2030/05/18 - 11:30:00 <- this is a valid date
+
+impl Date {
+
+}
